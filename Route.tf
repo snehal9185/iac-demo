c66c084d7eda41782b185eacb8e855a6e6e3bd36
snehal9185 <63599150+snehal9185@users.noreply.github.com>
2021-05-22 20:38:25 +0530

    Update Route.tf

--- Route.tf
+++ Route.tf
@@ -6,10 +6,7 @@ resource "aws_route_table" "example" {
     gateway_id = "igw-0504cdfea50e005ca"
   }
 
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = "igw-0504cdfea50e005ca"
-  }
+  
 
   tags = {
     Name = "example"
